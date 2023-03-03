55f03c917a33cd2e4779dfa8f525f8cf76f804ff
Mmesek <13630781+Mmesek@users.noreply.github.com>
2023-03-03 18:48:19 +0100

    Update blacklist processor logic
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- RSS/processors.py
+++ RSS/processors.py
@@ -1,7 +1,9 @@
-from .utils import Entry, processor
+from RSS.models import Feed_Post
+from RSS.utils import processor
+
 
 @processor
-def blacklist(entry: Entry, value: str) -> bool:
-    if value in entry.embed.description:
-        return False
-    return True
+def blacklist(post: Feed_Post, value: str) -> bool:
+    if value in post.content:
+        post.feed.posts.remove(post)
+        return True
