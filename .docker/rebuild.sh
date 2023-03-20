b2a29f244ae85dad9823b97f0f305ec1df4cb74b
Mmesek <13630781+Mmesek@users.noreply.github.com>
2023-03-20 15:45:26 +0100

    Switch to using network & expose port & Fix module
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- .docker/rebuild.sh
+++ .docker/rebuild.sh
@@ -2,6 +2,6 @@ docker stop RSS RSS_Client
 docker rm RSS RSS_Client
 docker build -f ./.docker/Dockerfile -t rss:latest .
 docker image prune
-docker create --link postgres --name RSS rss:latest
-docker create --link postgres --name RSS_Client --entrypoint python rss:latest -m rss.client
+docker create --network database --name RSS -p 8081:8080 rss:latest
+docker create --network database --name RSS_Client --entrypoint python rss:latest -m RSS.client
 docker start RSS
